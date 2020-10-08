25d4ddfad9cf569e2d67bf763f302ad65446684f
MarcosCarbonero <71753067+MarcosCarbonero@users.noreply.github.com>
2020-10-08 11:22:21 +0200

    Add files via upload

---
+++ C1iris.R
@@ -0,0 +1,11 @@
+library(datasets)
+?iris
+df<-iris 
+head(df)
+df
+hist(df$Petal.Width,main =" European University - Histogram of Petal with ",
+     xlab = "Petal width (in cm)")
+rm(list = ls())
+detach("package:datasets",unload =TRUE)
+graphics.off()
+cat("\014")
