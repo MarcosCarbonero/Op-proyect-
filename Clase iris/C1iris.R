242635e6c6591cc9668efe86f3c37c2971e538e1
MarcosCarbonero <marcosgomec9@gmail.com>
2020-10-08 12:06:46 +0200

    2 entrega

--- Clase iris/C1iris.R
+++ Clase iris/C1iris.R
@@ -5,4 +5,8 @@ head(df)
 df
 hist(df$Petal.Width,main =" European University - Histogram of Petal with ",
      xlab = "Petal width (in cm)")
+rm(list = ls())
+detach("package:datasets",unload = TRUE )
+graphics.off()
+cat("\014")
 
