94cd5a5089af716758317d60fa7f196cc27b1110
MarcosCarbonero <marcosgomec9@gmail.com>
2020-10-14 10:17:24 +0200

    verificar randomforest

---
+++ trabajoMigradoAr.R
@@ -0,0 +1,27 @@
+library(dplyr)
+d <- read.csv("./train.csv ")
+names(d)
+str(d)
+#### NOS QUEDAMOS CON LAS VARIABLES QUE NOS INTERESAN ####
+datos <-select( d,YearBuilt,TotalBsmtSF,OverallQual,GrLivArea,GarageArea,SalePrice)
+attach(datos)
+names(datos)
+str(datos)
+#VEMOS LA CORRELACIÓN CON LA VARIABLE ####
+cor.test(SalePrice,YearBuilt)
+cor.test(SalePrice,TotalBsmtSF)
+cor.test(SalePrice,OverallQual)
+cor.test(SalePrice,GrLivArea)
+cor.test(SalePrice,GarageArea)
+#MODELO DE REGRESION LINEAL#### 
+modeloRLineal <- lm(SalePrice ~ YearBuilt + TotalBsmtSF + OverallQual + GrLivArea +GarageArea )
+summary(modelo_multiple)
+##MODELO DE RANDOM FOREST####
+install.packages("randomForest")
+library(caret)
+library(randomForest)
+set.seed(2020)
+modeloRForest <- randomFores(x=datos[training.ids,1:5],
+                             y= datos[training.ids,5],
+                             ntree =500,
+                             keep.forest=TRUE)
